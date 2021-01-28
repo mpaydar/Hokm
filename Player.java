2b3fe06d3972f868fc97e5c040cefa898b0ec1ba
m21newyork@gmail.com <m21newyork@gmail.com>
2021-01-27 23:40:05 -0500

    Controller Added, Choose Teammate added, seeHand Mofified

--- Player.java
+++ Player.java
@@ -2,19 +2,32 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Player extends Node {
-	
+	// I need to use an array helper to let me know which of the 52 cards has been used
+	// so a 2D array with 4 row: each row representing a kind of card
+	// 					  13 column: each column represent ranks Ace to King
 	Node first=null;
 	String nameOfPlayer="";
 	Node last=null;
+	
 	Player ()
 	{
 		super();
+		player_number++;
 		System.out.print("Welcome, Enter Player Name: ");
 		Scanner object= new Scanner(System.in);
 		this.nameOfPlayer=object.next();
 		Node new_card= new Node();
 		this.first=new_card;
 		this.last=new_card;
+		
+	}
+	
+	void chooseTeamMate()
+	{
+		System.out.print("Which player you like to be your teammate?");
+		Scanner object=new Scanner(System.in);
+		String teamMate=object.nextLine();
+		System.out.println(this.nameOfPlayer + ", "  + teamMate + " is your teammate..");
 	}
 	
 	
@@ -24,6 +37,7 @@ public class Player extends Node {
 		for(int i=0;i<5;i++)
 		{
 			Node new_card=new Node();
+			new_card.player_number=player_number;
 			if(this.first!=null)
 			{
 				this.last.next=new_card;
@@ -32,7 +46,7 @@ public class Player extends Node {
 		}
 	}
 
-	boolean legitHand(Player l)
+	boolean legitHand()
 	{
 		Node temp=this.first.next;
 		while(temp.next.next!=null)
@@ -50,10 +64,12 @@ public class Player extends Node {
 		return true;
 		
 	}
-	void seeHand(Player l)
+	
+	void seeHand()
 	{
+		
 		Node temp=first.next;
-		boolean ok=legitHand(l);
+		boolean ok=legitHand();
 		while(temp!=null)
 		{
 			if(ok)
@@ -62,10 +78,30 @@ public class Player extends Node {
 				temp=temp.next;
 			}
 		}
+		System.out.println("");
 		
 	}
 	
-	
+	void controller()
+	{
+		
+		System.out.println(this.nameOfPlayer  + ", What action you like to take?");
+		System.out.println("                               1. See Your Cards"  );
+		System.out.println("                               2. choose Teammate"  );  								   
+		Scanner sc=new Scanner(System.in);
+		int action=sc.nextInt();
+		switch(action)
+		{
+		case 1:
+			handOutCard();
+			seeHand();
+			break;
+		case 2:
+			chooseTeamMate();
+			break;
+			
+		}
+	}
 
 		
 		
@@ -78,37 +114,11 @@ public class Player extends Node {
 	}
 	
 	
-	
-	void givePlayerOption()
-	{
-		System.out.println("Possible actions: 1. Organize Your Card  "+ "  2. Play as is");
-		
-		Scanner object=new Scanner(System.in);
-		int action= object.nextInt();
-		switch(action)
-		{
-		case 1:
-			SortHand();
-			break;
-		case 2:
-			System.out.print("Game will start in a moment...");
-			break;
-		}
-		
-		
-	}
+
 	
 	
 	
-	
-	public static void main(String args [])
-	{
-		Player Moe=new Player();
-		Moe.handOutCard();
-		Moe.seeHand(Moe);
-		Moe.givePlayerOption();
-		
-	}
+
 	
 
 }
